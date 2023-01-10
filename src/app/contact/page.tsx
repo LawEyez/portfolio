3f2a7d4822eea3ef6062b3314ef510a0e7bd5443
Bioga Allen <allenojuka10@gmail.com>
2023-01-10 11:28:20 +0300

    Add contact card and phone.

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,21 +1,32 @@
-import { RiMailUnreadFill } from 'react-icons/ri'
+import { RiMailUnreadFill, RiPhoneFill } from 'react-icons/ri'
 
 export default function Contact() {
   return (
     <section className="relative h-screen w-full">
       <div className="flex items-center h-full container">
-        <div className='w-full space-y-4 break-words'>
-          <p className="text-white font-bold w-full md:w-1/2 text-4xl tracking-tighter">
-            Let&apos;s Talk
+        <div className='w-full space-y-8 break-words'>
+          <p className="text-white font-bold w-full md:w-1/2 text-6xl tracking-tighter">
+            Don&apos;t be shy, say hi
           </p>
 
-          <div className="flex flex-col gap-2">
-            <div className="flex items-center gap-2">
-              <span className="text-lg text-neutral-400">
+          <div className="flex flex-col gap-4">
+            <div className="flex items-center gap-2 sm:gap-4 p-2 sm:p-4 border border-white/20
+            rounded-xl w-full md:w-[40%]">
+              <div className="text-2xl text-neutral-400 border-r border-white pr-2 sm:pr-4">
                 <RiMailUnreadFill />
-              </span>
+              </div>
               <span className="text-sm font-light">
                 ojuka.allen@gmail.com
+              </span>
+            </div>
+            
+            <div className="flex items-center gap-2 sm:gap-4 p-2 sm:p-4 border border-white/20
+            rounded-xl w-full md:w-[40%]">
+              <div className="text-2xl text-neutral-400 border-r border-white pr-2 sm:pr-4">
+                <RiPhoneFill />
+              </div>
+              <span className="text-sm font-light">
+                (+254) 711 383 990
               </span>
             </div>
           </div>
