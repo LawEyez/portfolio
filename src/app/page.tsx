8936b818ebc88473ca20568736976a2630fb8db2
LawEyez <allenojuka10@gmail.com>
2023-12-04 16:51:03 +0300

    Add projects.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,35 @@
 import Image from 'next/image'
 
+import Project from "@components/Project";
+
+import { projects } from "data";
+
 export default function Home() {
   return (
-    <section className="relative h-screen w-full">
-        <div className="absolute w-64 -rotate-90 translate-y-96">
-          <div className="uppercase text-neutral-900 text-9xl font-black
-          tracking-[-10px]">
-            developer
-          </div>
-          
-          <div className="uppercase text-neutral-900 text-8xl font-black
-          tracking-[-10px]">
-            designer
-          </div>
-
-          <div className="uppercase text-neutral-900 text-7xl font-black
-          tracking-[-10px]">
-            artist
-          </div>
+    <section className="relative min-h-screen w-full pb-40">
+      {/* *********** ROTATED TEXT *********** */}
+      <div className="absolute w-64 -rotate-90 translate-y-96">
+        <div className="uppercase text-neutral-900 text-9xl font-black
+        tracking-[-10px]">
+          developer
         </div>
-      <div className="relative flex items-center h-full container">
+        
+        <div className="uppercase text-neutral-900 text-8xl font-black
+        tracking-[-10px]">
+          designer
+        </div>
 
+        <div className="uppercase text-neutral-900 text-7xl font-black
+        tracking-[-10px]">
+          artist
+        </div>
+      </div>
+
+      {/* *********** CTA *********** */}
+      <div className="relative flex items-center h-screen container">
         <div className='w-full space-y-4 break-words'>
           <p className="text-neutral-300">Hi there, I&apos;m Allen, a...</p>
-          <div className="capitalize font-bold text-6xl flex flex-col lg:flex-row
+          <div className="capitalize font-bold text-7xl flex flex-col lg:flex-row
           tracking-tighter">
             <span className="">developer.</span>
             <span className="">designer.</span>
@@ -32,12 +38,33 @@ export default function Home() {
         </div>
       </div>
 
+      {/* *********** ABOUT *********** */}
       <div className='w-full space-y-4 break-words container pb-40'>
-        <p className="text-neutral-400 w-full md:w-1/2 text-xl">
+        <p className="text-neutral-400 w-full text-5xl">
           By day, a <span className="text-white">software engineer</span>, a <span className="text-white">UI/UX designer</span> and an aspiring <span className="text-white">entrepreneur</span>. By night, a <span className="text-white">writer</span>, <span className="text-white">artist</span>, and <span className="text-white">Batman&apos;s sidekick</span>.
         </p>
 
         <p className="text-neutral-400 text-sm"></p>
+      </div>
+
+      {/* *********** PROJECTS *********** */}
+      <div className="h-full container pt-20">
+        <h1 className="text-white w-full md:w-1/2 text-2xl font-black tracking-tighter">
+          Projects
+        </h1>
+        
+        <div className="mt-10">
+          {projects.map((project: any) => (
+            <Project
+              key={project.id}
+              name={project.name}
+              description={project.description}
+              tools={project.tools}
+              tags={project.tags}
+              url={project.url}
+            />
+          ))}
+        </div>
       </div>
     </section>
   )
