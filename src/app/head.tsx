5e326c2d29c5cd9d73dc407cefe671cdafaaeb67
LawEyez <allenojuka10@gmail.com>
2023-12-15 16:07:13 +0300

    Add g-tag.

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -1,3 +1,5 @@
+import Script from "next/script";
+
 export default function Head() {
   return (
     <>
@@ -5,6 +7,17 @@ export default function Head() {
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta name="description" content="Fullstack developer from Kenya." />
       <link rel="icon" href="/favicon.ico" />
+      {/* <!-- Google tag (gtag.js) --> */}
+      <Script async src="https://www.googletagmanager.com/gtag/js?id=G-3HE1DQV4F7" />
+      <Script id="google-analytics">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+        
+          gtag('config', 'G-3HE1DQV4F7');
+        `}
+      </Script>
     </>
   )
 }
