ae06311422ce819a78fc7876c343c609ee528dc6
LawEyez <allenojuka10@gmail.com>
2023-12-15 15:38:51 +0300

    Remove lines.

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -2,16 +2,9 @@ import Link from "next/link"
 
 const Menu = () => {
   return (
-    <>
-      <div className="flex items-center gap-1.5">
-        <div className="space-y-1">
-          <div className="h-0.5 w-6 bg-white" />
-          <div className="h-0.5 w-6 bg-white" />
-        </div>
-
-        <Link href='/' className="text-sm font-medium tracking-tighter">allen.</Link>
-      </div>
-    </>
+    <div className="flex items-center gap-1.5">
+      <Link href='/' className="text-xl font-medium tracking-tighter">allen.</Link>
+    </div>
   )
 }
 
